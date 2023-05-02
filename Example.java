cac181e4465df52fed224c6c9f8095a2d2abb6f5
lahiru randima <111424576+WIJETHUNGA-174@users.noreply.github.com>
2023-05-03 02:38:40 +0530

    Update Example.java
    Sampale commit

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("I AM WIJETHUNGA P.M.L.R.");
+        System.out.println("I AM WIJETHUNGA P.M.L.R.");
     }
 }
